d6a13516794389e46a9eb437ba8bc7919dcfe0ba
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-09-14 19:43:58 +0300

    Fixed width for header

--- src/components/styled/FullWidth.jsx
+++ src/components/styled/FullWidth.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FullWidth = ({ text }) => {
+const FullWidth = ({ text, viewBox }) => {
   return (
     <Wrapper>
-      <svg viewBox="0 0 136 15">
+      <svg viewBox={viewBox} width="100%">
         <text x="0" y="15">
           {text}
         </text>
@@ -14,11 +14,14 @@ const FullWidth = ({ text }) => {
 };
 
 const Wrapper = styled.div`
+  /* margin-bottom: 80px; */
   svg {
     width: 100%;
+    max-width: 100%;
   }
   text {
     font-weight: 500;
+    letter-spacing: 0.03em;
     text-transform: uppercase;
     fill: #fff;
     font-family: 'Gosha Sans', sans-serif;
